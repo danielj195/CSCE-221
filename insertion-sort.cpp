fb24eec4db55c0a690ad826a2e5ece2a2d13a6ab
danielj195 <danielj1@tamu.edu>
2016-09-25 20:36:01 -0500

    Add files via upload
    insertion fixed

--- insertion-sort.cpp
+++ insertion-sort.cpp
@@ -13,7 +13,7 @@ InsertionSort::sort(int A[], int size){
 	for (int k = 1; k < size; k++){
 		int tmp = A[k];
 		int j = k;
-		for (; j > 0 && (tmp < A[j - 1],num_cmps++); j--) {
+		for (; j > 0 && (num_cmps++, tmp < A[j - 1]); j--) {
 			//num_cmps++;
 			A[j] = A[j - 1];
 		}
